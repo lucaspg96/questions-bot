5005b9952880a00c8c1846926cd07b210f535199
Lucas Peres <lucaspg96@live.com>
2018-08-01 19:15:02 -0300

    Finishing first version

---
+++ Models.py
@@ -0,0 +1,51 @@
+from bson import ObjectId
+from pymongo import MongoClient
+#generic MongoDB document model
+class AbstractMongo(dict): 
+
+	__getattr__ = dict.get
+	__delattr__ = dict.__delitem__
+	__setattr__ = dict.__setitem__
+
+	#save in the database
+	def save(self):
+		#new document, must insert on database
+		if not self._id:
+			self.collection.insert(self)
+		#an existing document, must update
+		else:
+			self.collection.update(
+				{ "_id": ObjectId(self._id) }, self)
+
+	#load from the database
+	def reload(self):
+		if self._id:
+			self.update(self.collection\
+					.find_one({"_id": ObjectId(self._id)}))
+
+	#remove from the database
+	def remove(self):
+		if self._id:
+			self.collection.remove({"_id": ObjectId(self._id)})
+			self.clear()
+
+#question model
+class Question(AbstractMongo):
+	collection = MongoClient()["telegram"]["questions"]
+
+	@staticmethod
+	def get_oldest():
+		try:
+			doc = Question.collection.find().sort([("time",1)]).limit(1)[0]
+		
+			return Question(doc)
+
+		except Exception:
+			return None
+
+	def __str__(self):
+		return """
+Usuário: {} ({})
+Hora: {}
+
+Pergunta: {}""".format(self.user_name,self.user_tag,self.time,self.text)
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
                           ConversationHandler)
 from telegram import ParseMode
+from Models import Question
 import logging
+from datetime import datetime
+
+from pymongo import MongoClient
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 bot_token = ""
 bot_name = ""
+
 lecture_name = ""
 presenter_name = ""
+presenter_id = #integer
 
 default_message = """
 Olá! Sou o {}. Estou aqui para registrar perguntas para a palestra:
@@ -18,26 +24,63 @@ apresentada por <b>{}</b>.
 Para enviar uma pergunta, entre /question .
 """.format(bot_name, lecture_name, presenter_name)
 
-count = 0
+count = MongoClient()["telegram"]["questions"].count()
 
 def start(bot,update):
 	update.message.reply_text(default_message, parse_mode=ParseMode.HTML)
 
-
 def question_entry_point(bot,update):
-	update.message.reply_text("Por favor, entre sua pergunta ou digite /cancel para cancelar")
+	update.message.reply_text("Entrando no modo de perguntas. Cada mensagem que enviar será considerado uma pergunta. Digite /done para finalizar")
 	return "WAITING_QUESTION"
 
 def store_question(bot,update):
 	text = update.message.text
-	if text != None:
-		print("Has text")
-	else:
-		print("Has no text")
-	return ConversationHandler.END
 
-def cancel(bot,update):
-	update.message.reply_text("Pergunta descartada")
+	if text == "/done":
+			return done(bot,update)
+
+	if text[0] == "/":
+		update.message.reply_text("Enquanto no modo de perguntas, comandos serão desconsiderados, exceto pelo /done")
+		return "WAITING_QUESTION"
+
+	if text != None:
+
+		user = update.message.from_user
+		question = Question()
+		question["text"] = text
+		question["user_id"] = user.id
+		question["user_name"] = user.first_name + " " + user.last_name
+		question["user_tag"] = "@"+user.username if user.username else ""
+		question["time"] = datetime.now()
+
+		question.save()
+
+		global count
+		count += 1
+
+		update.message.reply_text("Obrigado. Sua mensagem foi adicionada na pilha na posição {}. Fique a vontade para mandar outra pergunta ou entre /done para finalizar."\
+			.format(count))
+
+	else:
+		update.message.reply_text("Por favor, entre uma pergunta em texto. Outros tipos de mensagens serão desconsiderados")
+	
+	return "WAITING_QUESTION"
+	
+
+def get_oldest_question(bot,update):
+	if update.message.from_user.id == presenter_id:
+		question = Question.get_oldest()
+		print(question)
+		if question:
+			update.message.reply_text(str(question))
+			question.remove()
+		else:
+			update.message.reply_text("Não existem perguntas a serem respondidas")
+	else:
+		update.message.reply_text("Perdão, mas você não tem permissão de consultar as perguntas")
+
+def done(bot,update):
+	update.message.reply_text("Obrigado!")
 	return ConversationHandler.END
 
 ################
@@ -52,13 +95,16 @@ conversation = ConversationHandler(
 	states={
 		"WAITING_QUESTION": [MessageHandler(Filters.all,store_question)]
 	},
-	fallbacks=[CommandHandler('cancel', cancel)]
+	fallbacks=[
+		CommandHandler('done', done)
+	]
 )
 
 dp.add_handler(conversation)
 
 dp.add_handler(CommandHandler("start", start))
-dp.add_handler(MessageHandler(Filters.text, start))
+dp.add_handler(CommandHandler("questions",get_oldest_question))
+dp.add_handler(MessageHandler(Filters.all, start))
 
 updater.start_polling()
 updater.idle()
